ccaa60ba387d700f8d0fa3dd71505a25f1da0cd1
肖乐 <lele.xiao@melot.cn>
2020-04-20 17:32:50 +0800

    Add project

---
+++ XLLJenkinsDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XLLJenkinsDemo
+//
+//  Created by xiaoll on 2020/4/20.
+//  Copyright © 2020 melot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ XLLJenkinsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XLLJenkinsDemo
+//
+//  Created by xiaoll on 2020/4/20.
+//  Copyright © 2020 melot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
